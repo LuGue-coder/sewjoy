d77d1da3b31db714c582f6bcbf80e7a519253e5a
LuGue-coder <lucka.n18@seznam.cz>
2025-10-22 17:23:36 +0200

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import CardComponent from "./components/CardComponent"
+import SideNav from "./components/SideNav"
+
+const App = () => {
+  return (
+    <>
+    <aside>
+      <SideNav/>
+    </aside>
+    <CardComponent/>
+    </>
+  )
+} 
+export default App
+
---
+++ frontend/src/components/SideNav.tsx
@@ -0,0 +1,11 @@
+import {Box, Text, } from "@chakra-ui/react"
+
+export default function SideNav() {
+  return (
+    <Box bg="honeydew" color="brown">
+        <Text >Projects</Text>
+        <Text >Fabrics</Text>
+    </Box>
+  )
+}
+
