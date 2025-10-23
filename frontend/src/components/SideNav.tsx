6391ff7e094158697221791cec59d305b809a607
LuGue-coder <lucka.n18@seznam.cz>
2025-10-23 20:30:20 +0200

    delete

--- frontend/src/components/SideNav.tsx
+++ frontend/src/components/SideNav.tsx
@@ -3,7 +3,8 @@ import {Flex, Text, } from "@chakra-ui/react"
 export default function SideNav() {
   return (
     <Flex  direction="column" alignItems="center"
-     bgColor="lightsalmon"  minHeight="100vh" paddingTop="6rem" color="brown" fontSize="1.2rem" >
+     bgColor="lightsalmon"  minHeight="100vh" paddingTop="6rem" color="brown" fontSize="1.2rem" 
+     >
         <Text padding="1rem" >Projects</Text>
         <Text >Fabrics</Text>
     </Flex>
--- frontend/src/data/types.tsx
+++
@@ -1,6 +0,0 @@
-export type Project = {
-  id: number;
-  name: string;
-  description?: string; // optional
-  image: string;
-}
